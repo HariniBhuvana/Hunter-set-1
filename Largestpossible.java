8fe1c7fb60841dcb956a5f55731017072e46eafd
Harini <harinee2015@gmail.com>
2017-07-05 15:16:29 +0530

    Create Largestpossible.java

---
+++ Largestpossible.java
@@ -0,0 +1,47 @@
+import java.util.*;
+import java.io.*;
+class Largestpossible
+{
+  public static void main(String[] args)
+  {
+    System.out.println("ENTER THE NUMBER OF ELEMENTS");
+    Scanner s=new Scanner(System.in);
+    int n=s.nextInt();
+    String[] z=new String[n];
+    System.out.println("ENTER THE ELEMENTS");
+    for(int x=0;x<n;x++)
+    {
+      z[x]=s.next();
+    }
+    for(int k=0;k<z.length;k++)
+    {
+      String temp=z[k];
+      for(int i=0;i<z.length;i++)
+      {
+        String temp1=z[i];
+        for(int j=0;j==0;)
+        {
+          if(temp.charAt(j)>temp1.charAt(j))
+          {
+            String swap=z[k];
+            z[k]=z[i];
+            z[i]=swap;
+            break;
+          }
+          else
+          break;
+        }
+      }
+    }
+    //StringBuilder StrNum=new StringBuilder();
+    System.out.println("LARGEST NUMBER POSSIBLE IS:");
+    
+    for(String out:z)
+    {
+      //StrNum.append(out);
+      System.out.print(out);
+    }
+    //int finalInt=Integer.parseInt(StrNum.toString());
+    //System.out.println("LARGEST NUMBER IS : "+finalInt);
+  }
+} 
